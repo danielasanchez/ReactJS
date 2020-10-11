2c32c22b63134c31aa5979cc2c44289a86961e14
Daniela Sanchez <danielasanchez.itt@hotmail.com>
2020-10-11 01:10:41 -0700

    example3, containers

---
+++ example3/src/App.js
@@ -0,0 +1,38 @@
+import React, {Component,Fragment} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <>
+        <h1>JavaScript</h1>
+        <h1>React</h1>
+        <h1>Native</h1>
+      </>
+  )};
+}
+
+/*
+const App=()=>{
+  return (
+      <Fragment>
+        <h1>JavaScript</h1>
+        <h1>React</h1>
+        <h1>Native</h1>
+      </Fragment>
+  );
+} 
+*/
+/*
+function App(){
+  return (
+      <>
+        <h1>JavaScript</h1>
+        <h1>React</h1>
+        <h1>Native</h1>
+      </>
+  );
+}
+*/
+export default App;
