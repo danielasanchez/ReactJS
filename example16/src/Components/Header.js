d0cd11677280a12c0498c4ba81ab74abdead0346
Daniela Sanchez <danielasanchez.itt@hotmail.com>
2020-10-31 01:30:42 -0700

    state, functions and props

---
+++ example16/src/Components/Header.js
@@ -0,0 +1,15 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+const Header = () => {
+    return (  
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Example #16
+        </p>
+      </header>
+    );
+}
+ 
+export default Header;
