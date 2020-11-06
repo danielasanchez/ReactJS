2445d27523878eaec7999c1cfa4eb26cfdaaaff5
Daniela Sanchez <danielasanchez.itt@hotmail.com>
2020-11-05 23:14:05 -0800

    Firebase

---
+++ example18/src/App.js
@@ -0,0 +1,165 @@
+import './App.css';
+import React, { Component } from 'react';
+import Swal from 'sweetalert2';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+import Formulario from './Components/Formulario';
+import Listado from './Components/Listado';
+import firebase from './Settings/ConfigFirebase.js'
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      alumno:{
+        matricula:"",
+        nombre:"",
+        correo:"",
+        carrera:""
+      },
+      lista:[],
+      desactivado:false,
+    };
+  }
+  
+  componentDidMount(){
+    firebase.database().ref('Alumnos').on('value', snapshot=> {
+      let alumnosLista=[];
+      snapshot.forEach(row=>{
+          alumnosLista.push({
+            matricula:row.key,
+            nombre:row.val().nombre,
+            correo:row.val().correo,
+            carrera:row.val().carrera
+          })
+      })
+      this.setState({
+        ...this.state,
+        lista:alumnosLista
+      })
+    });
+  }
+
+
+  guardarCambios=(e)=>{
+    this.setState({ 
+      ...this.state,
+      alumno:{
+      ...this.state.alumno,  
+      [e.target.name]: e.target.value
+      } 
+      //nombre:e.target.value 
+    });
+  }
+  
+  eliminar=(id)=>{
+
+    firebase.database().ref('Alumnos/' + id).set(null).then(() => {
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Eliminado',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    });
+
+    const temporal = this.state.lista.filter(a=>a.matricula!==id)
+    this.setState({
+      ...this.state,
+      lista:temporal
+    })
+  }
+
+  modificar=(id)=>{
+    const temporal = this.state.lista.find(a=>a.matricula===id);
+    this.setState({
+      ...this.state,
+      alumno:{
+        matricula:temporal.matricula,
+        nombre:temporal.nombre,
+        correo:temporal.correo,
+        carrera:temporal.carrera
+      },
+      desactivado:true
+    })
+  }
+  enviar=(e)=>{
+    e.preventDefault();
+
+    const {matricula,nombre, correo, carrera} = this.state.alumno;
+      
+    const vacios = (matricula.length===0 && nombre.length===0 && correo.length===0 && carrera.length===0) || carrera==="selecciona" 
+
+    if(!vacios){
+
+      firebase.database().ref('Alumnos/' + matricula).update(this.state.alumno).then(() => {
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Alumno agregado',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      });
+
+
+      
+      
+      let temporal = this.state.lista;
+
+      if(this.state.desactivado===true){
+        temporal= temporal.filter(a=>a.matricula!==matricula)
+      }
+
+      this.setState({
+        ...this.state,
+        lista:[...temporal,this.state.alumno],
+        alumno:{
+          matricula:"",
+          nombre:"",
+          correo:"",
+          carrera:""
+        },
+        desactivado:false
+      })
+    }
+    else{
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Llena todos los campos',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      return;
+    }
+    
+  }
+  
+  render() {
+    
+    return (
+      <div className="App">
+        <Header/>
+        <div className="Containers">
+          <Formulario
+              enviar={this.enviar}
+              guardarCambios={this.guardarCambios}
+              alumno={this.state.alumno}
+              desactivado={this.state.desactivado}
+          />
+          <Listado
+            lista={this.state.lista}
+            eliminar={this.eliminar}
+            modificar={this.modificar}
+          />
+          
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
