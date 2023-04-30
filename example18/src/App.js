ef670529073722060a3902c79b59ed978e3a568f
danielasanchez <danielasanchez.itt@hotmail.com>
2023-04-30 16:41:23 -0700

    Firebase (Update 2023)

--- example18/src/App.js
+++ example18/src/App.js
@@ -1,29 +1,49 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Swal from 'sweetalert2';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header';
 import Formulario from './Components/Formulario';
 import Listado from './Components/Listado';
-import firebase from './Settings/ConfigFirebase.js'
+import { firebase } from './Settings/ConfigFirebase';
+//import firebase from './Settings/ConfigFirebase';
+import { onValue, ref, set, update } from 'firebase/database';
 
+//Codigo en componente tipo clase al final
+function App(){
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      alumno:{
-        matricula:"",
-        nombre:"",
-        correo:"",
-        carrera:""
-      },
-      lista:[],
-      desactivado:false,
-    };
-  }
+  const [alumno, setAlumno] = useState({
+    matricula: "",
+    nombre: "",
+    correo: "",
+    carrera: ""
+  });
+
+  const [lista,setLista] = useState([]);
+
+  const [desactivado,setDesactivado] = useState(false)
   
-  componentDidMount(){
+  useEffect(() => {
+
+    let alumnosLista = [];
+    const dbRef = ref(firebase, 'Alumnos');
+    onValue(dbRef, (snapshot) => {
+        snapshot.forEach((row) => {
+            alumnosLista.push({
+                matricula: row.key,
+                nombre: row.val().nombre,
+                correo: row.val().correo,
+                carrera: row.val().carrera
+            })
+  
+        });
+        setLista(alumnosLista)
+    }, {
+        onlyOnce: true
+    });
+
+
+   /* Version anterior
     firebase.database().ref('Alumnos').on('value', snapshot=> {
       let alumnosLista=[];
       snapshot.forEach(row=>{
@@ -34,98 +54,115 @@ class App extends Component {
             carrera:row.val().carrera
           })
       })
-      this.setState({
-        ...this.state,
-        lista:alumnosLista
-      })
+    
+      setLista(alumnosLista)
     });
-  }
+    */
 
 
-  guardarCambios=(e)=>{
-    this.setState({ 
-      ...this.state,
-      alumno:{
-      ...this.state.alumno,  
-      [e.target.name]: e.target.value
-      } 
-      //nombre:e.target.value 
-    });
-  }
-  
-  eliminar=(id)=>{
+  }, []);
 
-    firebase.database().ref('Alumnos/' + id).set(null).then(() => {
-      Swal.fire({
-        position: 'center',
-        icon: 'error',
-        title: 'Eliminado',
-        showConfirmButton: false,
-        timer: 1500
-      })
-    });
 
-    const temporal = this.state.lista.filter(a=>a.matricula!==id)
-    this.setState({
-      ...this.state,
-      lista:temporal
+  const guardarCambios = (e) => {
+
+    setAlumno({
+      ...alumno,
+        [e.target.name]: e.target.value
     })
   }
 
-  modificar=(id)=>{
-    const temporal = this.state.lista.find(a=>a.matricula===id);
-    this.setState({
-      ...this.state,
-      alumno:{
-        matricula:temporal.matricula,
-        nombre:temporal.nombre,
-        correo:temporal.correo,
-        carrera:temporal.carrera
-      },
-      desactivado:true
-    })
-  }
-  enviar=(e)=>{
-    e.preventDefault();
+  const eliminar = (id) => {
 
-    const {matricula,nombre, correo, carrera} = this.state.alumno;
-      
-    const vacios = (matricula.length===0 && nombre.length===0 && correo.length===0 && carrera.length===0) || carrera==="selecciona" 
-
-    if(!vacios){
-
-      firebase.database().ref('Alumnos/' + matricula).update(this.state.alumno).then(() => {
+    set(ref(firebase, 'Alumnos/' + id), null)
+      .then(() => {
         Swal.fire({
           position: 'center',
-          icon: 'success',
-          title: 'Alumno agregado',
+          icon: 'error',
+          title: 'Eliminado',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      })
+      .catch((error) => {
+        // The write failed...
+      });
+
+    /*  Version anterior
+      firebase.database().ref('Alumnos/' + id).set(null).then(() => {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Eliminado',
           showConfirmButton: false,
           timer: 1500
         })
       });
+    */
+    const temporal = lista.filter(a => a.matricula !== id)
 
+    setLista(temporal)
+  }
 
-      
-      
-      let temporal = this.state.lista;
+  const modificar = (id) => {
+    const temporal = lista.find(a => a.matricula === id);
+     setAlumno({
+        matricula: temporal.matricula,
+        nombre: temporal.nombre,
+        correo: temporal.correo,
+        carrera: temporal.carrera
+      });
 
-      if(this.state.desactivado===true){
-        temporal= temporal.filter(a=>a.matricula!==matricula)
+      setDesactivado(true)
+  }
+  
+  const enviar = (e) => {
+    e.preventDefault();
+
+    const { matricula, nombre, correo, carrera } = alumno;
+
+    //const vacios = (matricula.length===0 && nombre.length===0 && correo.length===0 && carrera.length===0) || carrera==="selecciona" 
+    const vacios = (matricula.length === 0 || nombre.length === 0 || correo.length === 0 || carrera.length === 0 || carrera === "selecciona")
+
+    if (!vacios) {
+      update(ref(firebase, 'Alumnos/' + matricula), alumno)
+        .then(() => {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Alumno agregado',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        })
+
+      /*version anterior
+     firebase.database().ref('Alumnos/' + matricula).update(this.state.alumno).then(() => {
+       Swal.fire({
+         position: 'center',
+         icon: 'success',
+         title: 'Alumno agregado',
+         showConfirmButton: false,
+         timer: 1500
+       })
+     });
+     */
+      let temporal = lista;
+
+      if (desactivado === true) {
+        temporal = temporal.filter(a => a.matricula !== matricula)
       }
 
-      this.setState({
-        ...this.state,
-        lista:[...temporal,this.state.alumno],
-        alumno:{
-          matricula:"",
-          nombre:"",
-          correo:"",
-          carrera:""
-        },
-        desactivado:false
-      })
+      setLista([...temporal, alumno]);
+      setAlumno({
+          matricula: "",
+          nombre: "",
+          correo: "",
+          carrera: ""
+        });
+      setDesactivado(false)
+
     }
-    else{
+    else {
       Swal.fire({
         position: 'center',
         icon: 'error',
@@ -135,31 +172,233 @@ class App extends Component {
       })
       return;
     }
-    
   }
-  
-  render() {
-    
+
+
+
     return (
       <div className="App">
-        <Header/>
+        <Header />
         <div className="Containers">
           <Formulario
-              enviar={this.enviar}
-              guardarCambios={this.guardarCambios}
-              alumno={this.state.alumno}
-              desactivado={this.state.desactivado}
+            enviar={enviar}
+            guardarCambios={guardarCambios}
+            alumno={alumno}
+            desactivado={desactivado}
           />
           <Listado
-            lista={this.state.lista}
-            eliminar={this.eliminar}
-            modificar={this.modificar}
+            lista={lista}
+            eliminar={eliminar}
+            modificar={modificar}
           />
-          
+
         </div>
       </div>
     )
-  }
+  
 }
 
 export default App;
+
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       alumno: {
+//         matricula: "",
+//         nombre: "",
+//         correo: "",
+//         carrera: ""
+//       },
+//       lista: [],
+//       desactivado: false,
+//     };
+//   }
+
+//   componentDidMount() {
+//     let alumnosLista = [];
+//     const dbRef = ref(firebase, 'Alumnos');
+//     onValue(dbRef, (snapshot) => {
+//       snapshot.forEach((row) => {
+//         alumnosLista.push({
+//           matricula: row.key,
+//           nombre: row.val().nombre,
+//           correo: row.val().correo,
+//           carrera: row.val().carrera
+//         })
+
+//       });
+//       this.setState({
+//         ...this.state,
+//         lista: alumnosLista
+//       })
+//     }, {
+//       onlyOnce: true
+//     });
+
+//     /* Version anterior
+//     firebase.database().ref('Alumnos').on('value', snapshot=> {
+//       let alumnosLista=[];
+//       snapshot.forEach(row=>{
+//           alumnosLista.push({
+//             matricula:row.key,
+//             nombre:row.val().nombre,
+//             correo:row.val().correo,
+//             carrera:row.val().carrera
+//           })
+//       })
+    
+//       this.setState({
+//         ...this.state,
+//         lista:alumnosLista
+//       })
+//     });
+//     */
+//   }
+
+
+//   guardarCambios = (e) => {
+//     this.setState({
+//       ...this.state,
+//       alumno: {
+//         ...this.state.alumno,
+//         [e.target.name]: e.target.value
+//       }
+//       //nombre:e.target.value 
+//     });
+//   }
+
+//   eliminar = (id) => {
+
+//     set(ref(firebase, 'Alumnos/' + id), null)
+//       .then(() => {
+//         Swal.fire({
+//           position: 'center',
+//           icon: 'error',
+//           title: 'Eliminado',
+//           showConfirmButton: false,
+//           timer: 1500
+//         })
+//       })
+//       .catch((error) => {
+//         // The write failed...
+//       });
+
+//     /*  Version anterior
+//       firebase.database().ref('Alumnos/' + id).set(null).then(() => {
+//         Swal.fire({
+//           position: 'center',
+//           icon: 'error',
+//           title: 'Eliminado',
+//           showConfirmButton: false,
+//           timer: 1500
+//         })
+//       });
+//     */
+//     const temporal = this.state.lista.filter(a => a.matricula !== id)
+//     this.setState({
+//       ...this.state,
+//       lista: temporal
+//     })
+//   }
+
+//   modificar = (id) => {
+//     const temporal = this.state.lista.find(a => a.matricula === id);
+//     this.setState({
+//       ...this.state,
+//       alumno: {
+//         matricula: temporal.matricula,
+//         nombre: temporal.nombre,
+//         correo: temporal.correo,
+//         carrera: temporal.carrera
+//       },
+//       desactivado: true
+//     })
+//   }
+//   enviar = (e) => {
+//     e.preventDefault();
+
+//     const { matricula, nombre, correo, carrera } = this.state.alumno;
+
+//     //const vacios = (matricula.length===0 && nombre.length===0 && correo.length===0 && carrera.length===0) || carrera==="selecciona" 
+//     const vacios = (matricula.length === 0 || nombre.length === 0 || correo.length === 0 || carrera.length === 0 || carrera === "selecciona")
+
+//     if (!vacios) {
+//       update(ref(firebase, 'Alumnos/' + matricula), this.state.alumno)
+//         .then(() => {
+//           Swal.fire({
+//             position: 'center',
+//             icon: 'success',
+//             title: 'Alumno agregado',
+//             showConfirmButton: false,
+//             timer: 1500
+//           })
+//         })
+
+//       /*version anterior
+//      firebase.database().ref('Alumnos/' + matricula).update(this.state.alumno).then(() => {
+//        Swal.fire({
+//          position: 'center',
+//          icon: 'success',
+//          title: 'Alumno agregado',
+//          showConfirmButton: false,
+//          timer: 1500
+//        })
+//      });
+//      */
+//       let temporal = this.state.lista;
+
+//       if (this.state.desactivado === true) {
+//         temporal = temporal.filter(a => a.matricula !== matricula)
+//       }
+
+//       this.setState({
+//         ...this.state,
+//         lista: [...temporal, this.state.alumno],
+//         alumno: {
+//           matricula: "",
+//           nombre: "",
+//           correo: "",
+//           carrera: ""
+//         },
+//         desactivado: false
+//       })
+//     }
+//     else {
+//       Swal.fire({
+//         position: 'center',
+//         icon: 'error',
+//         title: 'Llena todos los campos',
+//         showConfirmButton: false,
+//         timer: 1500
+//       })
+//       return;
+//     }
+//   }
+
+//   render() {
+
+//     return (
+//       <div className="App">
+//         <Header />
+//         <div className="Containers">
+//           <Formulario
+//             enviar={this.enviar}
+//             guardarCambios={this.guardarCambios}
+//             alumno={this.state.alumno}
+//             desactivado={this.state.desactivado}
+//           />
+//           <Listado
+//             lista={this.state.lista}
+//             eliminar={this.eliminar}
+//             modificar={this.modificar}
+//           />
+
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
--- example18/src/Components/Formulario.js
+++ example18/src/Components/Formulario.js
@@ -1,53 +1,53 @@
 import '../App.css';
 
 const Formulario = (props) => {
-    return ( 
+    return (
         <div className="Form">
-        <form onSubmit={props.enviar}>
-        <div>
-          <label htmlFor="matricula">Matricula</label>
-          <input
-            type="text"
-            placeholder="Ejm. 18211154"
-            onChange={props.guardarCambios}
-            value={props.alumno.matricula}
-            name="matricula"
-            disabled={props.desactivado}
-          />
+            <form onSubmit={props.enviar}>
+                <div>
+                    <label htmlFor="matricula">Matricula</label>
+                    <input
+                        type="text"
+                        placeholder="Ejm. 18211154"
+                        onChange={props.guardarCambios}
+                        value={props.alumno.matricula}
+                        name="matricula"
+                        disabled={props.desactivado}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="nombre">Nombre</label>
+                    <input
+                        type="text"
+                        placeholder="Ejm. Juan Perez"
+                        onChange={props.guardarCambios}
+                        value={props.alumno.nombre}
+                        name="nombre"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Correo</label>
+                    <input
+                        type="email"
+                        placeholder="Ej. correo@correo.com"
+                        onChange={props.guardarCambios}
+                        value={props.alumno.correo}
+                        name="correo"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="carrera">Carrera</label>
+                    <select name="carrera" value={props.alumno.carrera} onChange={props.guardarCambios}>
+                        <option value="selecciona">selecciona</option>
+                        <option value="Informatica">Informatica</option>
+                        <option value="Sistemas">Sistemas</option>
+                        <option value="TICS">TICS</option>
+                    </select>
+                </div>
+                <button>Enviar</button>
+            </form>
         </div>
-        <div>
-          <label htmlFor="nombre">Nombre</label>
-          <input
-            type="text"
-            placeholder="Ejm. Juan Perez"
-            onChange={props.guardarCambios}
-            value={props.alumno.nombre}
-            name="nombre"
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Correo</label>
-          <input
-            type="email"
-            placeholder="Ej. correo@correo.com"
-            onChange={props.guardarCambios}
-            value={props.alumno.correo}
-            name="correo"
-          />
-        </div>
-        <div>
-          <label htmlFor="carrera">Carrera</label>
-          <select name="carrera" value={props.alumno.carrera} onChange={props.guardarCambios}>
-            <option value="selecciona">selecciona</option>
-            <option value="Informatica">Informatica</option>
-            <option value="Sistemas">Sistemas</option>
-            <option value="TICS">TICS</option>
-          </select>
-        </div>
-        <button>Enviar</button>
-        </form>
-      </div>
     );
 }
- 
+
 export default Formulario;
